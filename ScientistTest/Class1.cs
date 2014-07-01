8993024faca23bf84e31894ffbe123b897f576bf
idriss92 <idriss2004@hotmail.com>
2014-07-01 04:37:28 +0200

    1er commit

---
+++ ScientistTest/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ScientistTest;
+
+namespace ScientistTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void ShouldAdd()
+        {
+            var a = new Coeur();
+            var b = a.Addition(5, 6);
+            Assert.AreEqual(11, b);
+        }
+        
+
+        [Test]
+        public void ShouldDivide()
+        {
+            var a = new Coeur();
+            var b = a.Division(4, 2);
+            Assert.AreEqual(2, b);
+        }
+        
+
+        [Test]
+        public void ShouldMult()
+        {
+            var a = new Coeur();
+            var b = a.Multiplication(5, 6);
+            Assert.AreEqual(30, b);
+        }
+
+
+        [Test]
+        public void ShouldSous()
+        {
+            var a = new Coeur();
+            var b = a.Soustraction(7, 6);
+            Assert.AreEqual(1, b);
+        }
+    }
+}
---
+++ ScientistTest/Coeur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScientistTest
+{
+    class Coeur
+    {
+        public double Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public double Division(int a, int b)
+        {
+            return a / b;
+        }
+        public double Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public double Soustraction(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
